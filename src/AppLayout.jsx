ffd7d174a911f25cd3d3c7b51cd8807028356940
Alliette Dortelus <allietted@gmail.com>
2023-09-10 22:04:07 -0400

    added layout

---
+++ src/AppLayout.jsx
@@ -0,0 +1,14 @@
+import NavMenu from './NavMenu';
+import Main from './Main';
+import Footer from './Footer';
+
+export default function AppLayout({children}) {
+    return(
+        <>
+        <NavMenu/>
+        <Main/>
+        {children}
+        <Footer/>
+        </>
+    )
+}
