359081c3760e85acf0727e24e65077c04c56edfb
Alliette Dortelus <allietted@gmail.com>
2023-09-12 04:17:22 -0400

    added layout

---
+++ src/layout/Main/Main.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Main () {
+    return(
+        <main>
+            <Container>
+                <Row>
+                    <Col>
+                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius iure, magni placeat accusantium rerum impedit maxime accusamus enim minus iste repellat similique atque nihil doloribus, animi corrupti? Quibusdam, iusto expedita!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nulla quaerat, neque temporibus commodi ipsam aliquid rerum, dignissimos excepturi officia, autem ipsa eum id minus? Mollitia repellendus dicta perspiciatis. Consequatur.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </main>
+    )
+}
